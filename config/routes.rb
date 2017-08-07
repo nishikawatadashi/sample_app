08e759335e61e7e3af5e9d67605a5306670b625c
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2017-08-07 16:55:52 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root 'application#home' # rootURLをapplicationコントローラーのhelloアクションに結びつけ
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/help' # static_pages/homeというURL(get)をstatic_pages_controllerのhomeアクションに結びつける
-  get 'static_pages/contact'
+  root 'static_pages#home'
+  get '/help', to: 'static_pages#help'   # static_pages/homeというURL(get)をstatic_pages_controllerのhomeアクションに結びつける
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,30 +2,26 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup # 各テストが行われる前に実行されるメソッド
-    @base_title = 'Ruby on Rails Tutorial Sample App' # オブジェクトの中の変数には＠をつける
-  end
-
-  test 'should get home' do
-    get static_pages_home_url
+  test "should get home" do
+    get root_path
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
-  test 'should get help' do
-    get static_pages_help_url
+  test "should get help" do
+    get help_path
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
-  test 'should get about' do
-    get static_pages_about_url
+  test "should get about" do
+    get about_path
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end

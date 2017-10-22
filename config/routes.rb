d87ff97037fc1e1a45bffca0f58c4aee112b6945
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2017-10-23 04:25:30 +0900

    Fix Account Activations2

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,19 @@ Rails.application.routes.draw do
 
   get 'password_resets/edit'
 
+  get 'sessions/new'
+
   root 'static_pages#home'
-  get '/help', to: 'static_pages#help'   # static_pages/homeというURL(get)をstatic_pages_controllerのhomeアクションに結びつける
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  get '/signup',  to: 'users#new'
-  post '/signup', to: 'users#create'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  get  '/help',    to: 'static_pages#help'   # static_pages/homeというURL(get)をstatic_pages_controllerのhomeアクションに結びつける
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
   resources :users do
     member do
       get :following, :followers
